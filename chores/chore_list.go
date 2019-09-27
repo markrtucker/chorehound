fbed49242b05f611d99f6ff3dc4b01360ce7fe7e
Mark Tucker <mark.tucker@us.ibm.com>
2019-09-27 08:41:05 -0400

    Refactor

--- chores/chore_list.go
+++ chores/chore_list.go
@@ -1,17 +1,12 @@
-package main
+package chores
 
 import "time"
 
-type chore struct {
+// Chore TODO godoc
+type Chore struct {
 	ID        string        `json:"id"`        // TODO: UUID
 	Name      string        `json:"name"`      // Name of the chore (e.g., "Take out the trash")
 	Frequency time.Duration `json:"frequency"` // TODO: custom type? Part of schedule??
 	Schedule  string        `json:"schedule"`  // TODO: proper type - list of people
 	Current   string        `json:"current"`   // TODO: Person. Part of schedule??
 }
-
-// Use cases:
-// Create a new chore and store it in bucket (S3/OS??)
-// Given a chore ID, read the chore JSON from the bucket
-// Given a chore, mark it as done (advance the schedule)
-// Given a chore, hound the person scheduled to do it
--- main.go
+++ main.go
@@ -15,3 +15,11 @@ func HandleRequest(ctx context.Context) (string, error) {
 func main() {
 	lambda.Start(HandleRequest)
 }
+
+// TODO: See https://ewanvalentine.io/how-im-writing-serverless-services-in-golang-these-days/
+
+// Use cases:
+// Create a new chore and store it in bucket (S3/OS??)
+// Given a chore ID, read the chore JSON from the bucket
+// Given a chore, mark it as done (advance the schedule)
+// Given a chore, hound the person scheduled to do it
